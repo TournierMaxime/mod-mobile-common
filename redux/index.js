1bd1b9a7b4d35a842ed8634b1d942469d625ed7a
Maxime Tournier <mtournier@devsdefrance.com>
2024-02-16 09:41:55 +0100

    remove auth

--- redux/index.js
+++ redux/index.js
@@ -1,20 +1,7 @@
-import {
-  authReducer,
-  confirmEmailReducer,
-  passwordReducer,
-  registerUserReducer
-} from './reducers/auth'
 import { favoritesReducer } from "./reducers/favorites"
-
-const authCommonReducer = {
-  auth: authReducer,
-  register: registerUserReducer,
-  password: passwordReducer,
-  confirmEmail: confirmEmailReducer
-}
 
 const favoriteReducer = {
   favorites: favoritesReducer
 }
 
-export { authCommonReducer, favoriteReducer }
+export { favoriteReducer }
