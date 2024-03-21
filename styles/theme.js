9d5d44ed7ac6aa6d85cc3d86e7bc3341eadad5bc
Maxime Tournier <mtournier@devsdefrance.com>
2024-03-21 15:27:54 +0100

    dark mode

--- styles/theme.js
+++ styles/theme.js
@@ -2,7 +2,9 @@ const useDynamicThemeStyles = (darkMode) => ({
   background: darkMode ? `bg-slate-800` : `bg-white`,
   text: darkMode ? `text-white` : `text-black`,
   colorIcon: darkMode ? `white` : `black`,
-  backgroundTabColor: darkMode ? `rgb(30 41 59)` : `white`
-});
+  backgroundTabColor: darkMode ? `rgb(30 41 59)` : `white`,
+  borderColor: darkMode ? `border-slate-700` : `border-slate-100`,
+  activeIcon: darkMode ? `teal` : `blue`,
+})
 
-export {useDynamicThemeStyles }
+export { useDynamicThemeStyles }
